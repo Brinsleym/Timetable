354f0a86bd9fae19a04b46bea6d7883d546b01b6
Brinsley Morrison <36922985+Brinsleym@users.noreply.github.com>
2018-03-05 13:55:41 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,31 @@
+modules: [
+	{
+		module: "timetable",
+		position: "center,	// This can be any of the regions. Best results in center regions.
+		config: {
+
+			feeds: [
+				{
+					title: "Monday",
+					url: "http://google.com/page.xml",
+				},
+				{
+					title: "Tuesday",
+					url: "http://google.com/page.xml",
+        },
+        {
+          title: "Wednesday",
+          url: "http://google.com/page.xml"
+        },
+        {
+          title: "Thursday",
+          url: "http://google.com/page.xml"
+        },
+        {
+          title: "Friday",
+          url: "http://google.com/page.xml"
+        },  
+			]
+		}
+	}
+]
